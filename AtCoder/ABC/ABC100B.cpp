59ca0fcee5e3eeb0a0e734ae6aebcfe6e32d723d
Keiichiro Fujimori <dormire1010@gmail.com>
2020-04-02 12:15:35 +0900

    20200402_streak

---
+++ AtCoder/ABC/ABC100B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for (long long i = 0; i < (long long)n; i++)
+#define _GLIBCXX_DEBUG
+using namespace std;
+using ll = long long;
+using Pi = pair<int, int>;
+using Pl = pair<ll, ll>;
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  int D, N;
+  cin >> D >> N;
+  if (N == 100) N++;
+  cout << (ll)pow(100, D) * N << endl;
+}
