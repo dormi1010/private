482dfd0b6f003986203ff4ba19b05288e64d5f6b
Keiichiro Fujimori <dormire1010@gmail.com>
2020-05-06 01:18:14 +0900

    20200506_streak

--- AtCoder/ABC/ABC166/F/ABC166F.cpp
+++ AtCoder/ABC/ABC166/F/ABC166F.cpp
@@ -15,33 +15,60 @@ int main() {
   // std::cout << std::fixed << std::setprecision(15); //"for float-base
   // problem"
   ll N, A, B, C;
-  cin >> N >> A >> B >> C;
+  Vl V(3);
+  cin >> N >> V[0] >> V[1] >> V[2];
   vector<string> S(N);
+  vector<char> Ans;
   REP(i, N)
   cin >> S.at(i);
   REP(j, N) {
-    ll m, n;
-    if (S.at(j) == "AB") {
-      m = &A;
-      n = &B;
-    } else if (S.at(j) == "BC") {
-      m = &B;
-      n = &C;
-    } else {
-      m = &A;
-      n = &C;
-    }
-    if (m == 0 && n == 0) {
+    ll L, R;
+    L = V.at((int)S.at(j).at(0) - 'A');
+    R = V.at((int)S.at(j).at(1) - 'A');
+    if (L == 0 && R == 0) {
       cout << "No" << endl;
       return 0;
-    } else if (A + B + C == 1 && m == 1 && n == 0) {
-      m--;
-      n++;
-      continue;
-    } else if (A + B + C == 1 && m == 0 && n == 1) {
-      m++;
-      n--;
-      continue;
-    } else if (j + 1 == N || m != 0 && m != 0)
+    } else if (R == 0) {
+      L--;
+      R++;
+      Ans.push_back(S.at(j).at(1));
+    } else if (L == 0) {
+      L++;
+      R--;
+      Ans.push_back(S.at(j).at(0));
+    } else if (V[0] + V[1] + V[2] == 2 && j != N - 1) {
+      if (S.at(j + 1).at(0) == S.at(j).at(0) ||
+          S.at(j + 1).at(1) == S.at(j).at(0)) {
+        L++;
+        R--;
+        Ans.push_back(S.at(j).at(0));
+      } else {
+        if (L >= R) {
+          L--;
+          R++;
+          Ans.push_back(S.at(j).at(1));
+        } else {
+          L++;
+          R--;
+          Ans.push_back(S.at(j).at(0));
+        }
+      }
+    } else {
+      if (L >= R) {
+        L--;
+        R++;
+        Ans.push_back(S.at(j).at(1));
+      } else {
+        L++;
+        R--;
+        Ans.push_back(S.at(j).at(0));
+      }
+    }
+    V.at((int)S.at(j).at(0) - 'A') = L;
+    V.at((int)S.at(j).at(1) - 'A') = R;
   }
+
+  cout << "Yes" << endl;
+  REP(k, N) cout << Ans.at(k) << endl;
+  return 0;
 }
