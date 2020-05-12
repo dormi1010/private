88bfc50c80002f52e8e61b2a795aed357f94e7f8
Keiichiro Fujimori <dormire1010@gmail.com>
2020-05-12 09:55:14 +0900

    0512asakatsu

--- AtCoder/ARC/ARC057A.cpp
+++ AtCoder/ARC/ARC057A.cpp
@@ -1,24 +1,25 @@
-#include<bits/stdc++.h>
-#define REP(i,n) for(int i=0;i<n;i++)
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n; i++)
 #define _GLIBCXX_DEBUG
 using namespace std;
 using ll = long long;
-using Pi = pair<int,int>;
-using Pl = pair<ll,ll>;
+using Pi = pair<int, int>;
+using Pl = pair<ll, ll>;
 
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-    ll A,K;
-    cin >> A >> K;
-    ll ans =0;
-    if (K==0)cout << 2000000000000-A<<endl;
-    else {
-        while(A < 2000000000000){
-        A+=(K*A);
-        A++;
-        ans++;
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  ll A, K;
+  cin >> A >> K;
+  ll ans = 0;
+  if (K == 0)
+    cout << 2000000000000 - A << endl;
+  else {
+    while (A < 2000000000000) {
+      A += (K * A);
+      A++;
+      ans++;
     }
     cout << ans << endl;
-    }
-    }
+  }
+}
