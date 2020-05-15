d65afad096be31396629d61981732a519b6d027e
dormi1010 <dormire1010@gmail.com>
2020-05-15 22:11:17 +0900

    20200515_streak

--- AtCoder/ABC/ABC167/F/ABC167F.cpp
+++ AtCoder/ABC/ABC167/F/ABC167F.cpp
@@ -1,23 +1,15 @@
 #include <bits/stdc++.h>
 #define REP(i, n) for (long long i = 0; i < (long long)n; i++)
-//#define _GLIBCXX_DEBUG
 using namespace std;
-using ll = long long;
-using Pi = pair<int, int>;
-using Pl = pair<ll, ll>;
-using Vl = vector<ll>;
-using vi = vector<int>;
-using Graph = vector<vi>;
 
 int main() {
   cin.tie(0);
   ios::sync_with_stdio(false);
-  // std::cout << std::fixed << std::setprecision(15); //"for float-base
-  // problem"
   int N, L = 0;
   int neut_max = 0;
-  vector<pair<int, int>> positive;  // 余った'('のほうが多い文字列
-  vector<pair<int, int>> negative;  // 余った')'のほうが多い文字列
+  vector<pair<int, int>> positive;  // 余った'('のほうが多い文字列たち
+  vector<pair<int, int>> negative;  // 余った')'のほうが多い文字列たち
+  //同数のやつは後ろに影響しないのでLが最大の場所(positiveとnegativeの間）におく
   cin >> N;
   REP(i, N) {
     string S;
@@ -39,8 +31,9 @@ int main() {
     else if (r - l > 0)
       negative.push_back(make_pair(
           l, r));  // l == 0の文字列が最後に来ないと破滅、それ以外は自由
-    else if (r - l == 0 && r > neut_max)  //こいつはpositiveとnegativeの間におく,
-                                          //rが最大のやつ以外は空気
+    else if (r - l == 0 &&
+             r > neut_max)  //こいつはpositiveとnegativeの間におく,
+                            // rが最大のやつ以外は空気
       neut_max = r;
   }
   sort(positive.begin(), positive.end());
@@ -54,13 +47,13 @@ int main() {
   }
 
   if (neut_max > L) {
-    cout << "No" << endl;
+    cout << "No" << endl;  //左右同数だけど')'が多すぎて相殺できない
     return 0;
   }
 
   for (auto n : negative) {
     if (n.second > L) {
-      cout << "No" << endl;
+      cout << "No" << endl;  //余った')'が残りの'('の数を上回った
       return 0;
     }
     L -= n.second;
@@ -70,5 +63,5 @@ int main() {
   if (L == 0)
     cout << "Yes" << endl;
   else
-    cout << "No" << endl;
+    cout << "No" << endl;  //'('が余っちゃった
 }
